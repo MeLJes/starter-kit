24f82c0a156d39f8c46912d18912565751265587
MeLJes <meljesx@gmail.com>
2019-08-26 21:55:57 +0300

    Core update for 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,23 @@
 const gulp = require('gulp');
-const watch = require('gulp-watch');
 const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
-const { resolve } = require('path');
+const browserSync = require('browser-sync').create();
 
-// Path, related to the root,
-// where both .css and .scss style files will be stored
-const ROOT = './';
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-gulp.task('sass', function () {
-  if (!isProduction) {
-    return gulp.src(resolve(ROOT, 'scss/main.scss'))
-      .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(rename('style.css'))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(resolve(ROOT, 'css')));
+const paths = {
+  styles: {
+    src: './scss/**/*.scss',
+    dest: 'css/'
+  },
+  scripts: {
+    src: './js/**/*.js',
+    dest: 'js/'
   }
+};
 
-  return gulp.src(resolve(ROOT, 'scss/main.scss'))
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 5 versions']
-    }))
-    .pipe(rename('style.css'))
-    .pipe(gulp.dest(resolve(ROOT, 'css')));
-});
-
-gulp.task('sass:watch', function () {
-  watch(resolve(ROOT, 'scss/**/*.scss'), function () {
-    gulp.start('sass');
-  });
-});
-
-gulp.task('build', function () {
-  if (isProduction) {
-    del('**/style.css.map').then(function () {
-      gulp.start('sass');
-    });
-  }
-});
-
-gulp.task('default', ['sass:watch']);
+// --- Default
+function style() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+exports.style = style;
